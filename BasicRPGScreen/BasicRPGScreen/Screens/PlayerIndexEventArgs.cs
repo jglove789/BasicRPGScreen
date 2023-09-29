723c96d064a0c1270aba4ccf0796ef810f8f4bb8
jglove789 <jglove789@ksu.edu>
2023-09-29 18:28:38 -0500

    Big changes made to the architecture of the game. Still a WIP but it is almost done I think.

---
+++ BasicRPGScreen/BasicRPGScreen/Screens/PlayerIndexEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicRPGScreen.Screens
+{
+    // Custom event argument which includes the index of the player who
+    // triggered the event. This is used by the MenuEntry.Selected event.
+    public class PlayerIndexEventArgs
+    {
+        public PlayerIndex PlayerIndex { get; }
+
+        public PlayerIndexEventArgs(PlayerIndex playerIndex)
+        {
+            PlayerIndex = playerIndex;
+        }
+    }
+}
