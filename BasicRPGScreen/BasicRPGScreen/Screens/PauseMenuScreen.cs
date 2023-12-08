9b67ba80aded21919b4ef636238dce3d331615c5
jglove789 <jglove789@ksu.edu>
2023-12-07 19:00:49 -0600

    Implemented more options in the pause menu.

--- BasicRPGScreen/BasicRPGScreen/Screens/PauseMenuScreen.cs
+++ BasicRPGScreen/BasicRPGScreen/Screens/PauseMenuScreen.cs
@@ -11,14 +11,52 @@ namespace BasicRPGScreen.Screens
         public PauseMenuScreen() : base("Paused")
         {
             var resumeGameMenuEntry = new MenuEntry("Resume Game");
-            var quitGameMenuEntry = new MenuEntry("Quit Game");
+            var characterGameMenuEntry = new MenuEntry("Character Info");
+            var inventoryGameMenuEntry = new MenuEntry("Inventory and Equipment");
+            var saveGameMenuEntry = new MenuEntry("Save");
+            var quitGameMenuEntry = new MenuEntry("Save and Quit Game");
 
             resumeGameMenuEntry.Selected += OnCancel;
+            characterGameMenuEntry.Selected += CharacterGameMenuEntrySelected;
+            inventoryGameMenuEntry.Selected += InventoryGameMenuEntrySelected;
+            saveGameMenuEntry.Selected += SaveGameMenuEntrySelected;
             quitGameMenuEntry.Selected += QuitGameMenuEntrySelected;
 
             MenuEntries.Add(resumeGameMenuEntry);
+            MenuEntries.Add(characterGameMenuEntry);
+            MenuEntries.Add(inventoryGameMenuEntry);
+            MenuEntries.Add(saveGameMenuEntry);
             MenuEntries.Add(quitGameMenuEntry);
         }
+
+
+        private void CharacterGameMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        {
+            ExitScreen();
+        }
+
+
+        private void InventoryGameMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        {
+            ExitScreen();
+        }
+
+
+        private void SaveGameMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        {
+            const string message = "Save Game?";
+            var saveGameMessageBox = new MessageBoxScreen(message);
+
+            saveGameMessageBox.Accepted += ConfirmedSaveMessageBoxAccepted;
+
+            ScreenManager.AddScreen(saveGameMessageBox, ControllingPlayer);
+        }
+
+        private void ConfirmedSaveMessageBoxAccepted(object sender, PlayerIndexEventArgs e)
+        {
+            ScreenManager.SaveGame();
+        }
+
 
         private void QuitGameMenuEntrySelected(object sender, PlayerIndexEventArgs e)
         {
@@ -33,6 +71,7 @@ namespace BasicRPGScreen.Screens
         // This uses the loading screen to transition from the game back to the main menu screen.
         private void ConfirmQuitMessageBoxAccepted(object sender, PlayerIndexEventArgs e)
         {
+            ScreenManager.SaveGame();
             LoadingScreen.Load(ScreenManager, false, null, new MainMenuScreen());
         }
     }
